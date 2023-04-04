9323c87eab48fdf4f8edb73d870b74ca549d6af5
jwc20 <choij2015@gmail.com>
2023-04-04 15:31:07 -0700

    Create simple base for widget app.

---
+++ 2_pyqt6_widgets.py/widget1.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt6.QtWidgets import (
+    QMainWindow, QApplication,
+    QLabel, QCheckBox, QComboBox, QListWidget, QLineEdit,
+    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider
+)
+from PyQt6.QtCore import Qt
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My App")
+
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec()
