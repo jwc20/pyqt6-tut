3905289475bb43869abb7ea6e78e4c2bf7dfbe8c
jwc20 <choij2015@gmail.com>
2023-04-04 14:40:10 -0700

    Create slot for button being pressed.

--- 1_signal_slots_and_events/app.py
+++ 1_signal_slots_and_events/app.py
@@ -1,35 +1,41 @@
-import sys 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton 
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
-class MainWindow(QMainWindow): 
-    def __init__(self): 
-        super().__init__() 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
         self.button_is_checked = True
 
-        self.setWindowTitle("My App") 
+        self.setWindowTitle("My App")
 
-        button = QPushButton("Press Me!") 
-        button.setCheckable(True)
-        # button.clicked.connect(self.the_button_was_clicked) 
-        button.toggled.connect(self.the_button_was_toggled)
-        button.setChecked(self.button_is_checked)   
+        self.button = QPushButton("Press Me!")
+        self.button.setCheckable(True)
+        # self.button.clicked.connect(self.the_button_was_clicked)
+        # self.button.toggled.connect(self.the_button_was_toggled)
+        self.button.released.connect(self.the_button_was_released)
+        self.button.setChecked(self.button_is_checked)
 
-        # Set the central widget of the Window. 
-        self.setCentralWidget(button) 
+        # Set the central widget of the Window.
+        self.setCentralWidget(self.button)
 
     # Custom slot
-    def the_button_was_clicked(self): 
-        print("Clicked!")   
+    def the_button_was_clicked(self):
+        print("Clicked!")
 
-    def the_button_was_toggled(self, checked): 
+    def the_button_was_toggled(self, checked):
         self.button_is_checked = checked
-        print("Checked?", checked) 
+        print("Checked?", checked)
 
-app = QApplication(sys.argv) 
-window = MainWindow() 
+    def the_button_was_released(self):
+        self.button_is_checked = self.button.isChecked()
+        print(self.button_is_checked)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
 
 window.show()
- 
+
 app.exec()
