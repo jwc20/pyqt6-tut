3288af18db6557864301cb84af066e5eb38e6020
jwc20 <choij2015@gmail.com>
2023-04-04 14:31:02 -0700

    Create a custom python button method (created custom slot named the_button_was_clicked which accepts the "clicked" signal from the QPushButton).

--- 1_signal_slots_and_events/app.py
+++ 1_signal_slots_and_events/app.py
@@ -7,6 +7,16 @@ class MainWindow(QMainWindow):
         super().__init__() 
         self.setWindowTitle("My App") 
 
+        button = QPushButton("Press Me!") 
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked) 
+
+        # Set the central widget of the Window. 
+        self.setCentralWidget(button) 
+
+    def the_button_was_clicked(self): 
+        print("Clicked!")   
+
 app = QApplication(sys.argv) 
 window = MainWindow() 
 
