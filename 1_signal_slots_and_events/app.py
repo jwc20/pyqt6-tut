6ff4533c2cd65f4f032a42749fda3f1994befb16
jwc20 <choij2015@gmail.com>
2023-04-04 14:48:33 -0700

    Update code to make the window contain a button which updates the window title randomly from a list called "window_titles" and disable itself if the window title is "Something went wrong."

--- 1_signal_slots_and_events/app.py
+++ 1_signal_slots_and_events/app.py
@@ -1,45 +1,53 @@
-import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+import sys
+from random import choice
+
+window_titles = [
+    "My App",
+    "My App",
+    "Still My App",
+    "Still My App",
+    "What on earth",
+    "What on earth",
+    "This is surprising",
+    "This is surprising",
+    "Something went wrong",
+]
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.button_is_checked = True
+        self.n_times_clicked = 0
 
         self.setWindowTitle("My App")
 
         self.button = QPushButton("Press Me!")
-        # self.button.setCheckable(True)
         self.button.clicked.connect(self.the_button_was_clicked)
-        # self.button.toggled.connect(self.the_button_was_toggled)
-        # self.button.released.connect(self.the_button_was_released)
-        # self.button.setChecked(self.button_is_checked)
+
+        self.windowTitleChanged.connect(self.the_window_title_changed)
 
         # Set the central widget of the Window.
         self.setCentralWidget(self.button)
 
-    # Custom slot
     def the_button_was_clicked(self):
-        # print("Clicked!")
-        self.button.setText("You already clicked me!")
-        self.button.setEnabled(False)
+        print("Clicked.")
+        new_window_title = choice(window_titles)
+        print("Setting title:  %s" % new_window_title)
+        self.setWindowTitle(new_window_title)
 
-        self.setWindowTitle("My Oneshot App")
+    def the_window_title_changed(self, window_title):
+        print("Window title changed: %s" % window_title)
 
-    # def the_button_was_toggled(self, checked):
-    #     self.button_is_checked = checked
-    #     print("Checked?", checked)
-
-    # def the_button_was_released(self):
-    #     self.button_is_checked = self.button.isChecked()
-    #     print(self.button_is_checked)
+        if window_title == "Something went wrong":
+            self.button.setDisabled(True)
 
 
 app = QApplication(sys.argv)
-window = MainWindow()
 
+window = MainWindow()
 window.show()
 
 app.exec()
