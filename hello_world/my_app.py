34d9f1ca1bff780e842a39c02c636327196bad95
jwc20 <choij2015@gmail.com>
2023-04-04 13:59:21 -0700

    Update to define a main window with a single button as it's central widget.

--- hello_world/my_app.py
+++ hello_world/my_app.py
@@ -1,15 +1,28 @@
-from PyQt6.QtWidgets import QApplication, QWidget 
-
 # Needed for access to command line arguments.
 import sys 
+
+from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+
+class MainWindow(QMainWindow): 
+    def __init__(self): 
+        # Call the constructor of the parent class. (QMainWindow) to set up the main window.
+        super().__init__() 
+        self.setWindowTitle("My App") 
+        button = QPushButton("Press Me!") 
+
+        # Set the central widget of the main window to be the button.
+        self.setCentralWidget(button) 
+
 
 # Create the application object. Need only one QApplication object per application. 
 # sys.argv is a list of command line arguments. 
 app = QApplication(sys.argv) 
 
 # Create a window object. Need only one QWidget object per window.
-window = QWidget()
-
+window = MainWindow()
 
 # Show the window. 
 window.show() 
