c08a705e435d88ca66e25e3590e33ea3616e55b2
jwc20 <choij2015@gmail.com>
2023-04-04 14:33:09 -0700

    Create another slot for button toggle.

--- 1_signal_slots_and_events/app.py
+++ 1_signal_slots_and_events/app.py
@@ -10,12 +10,17 @@ class MainWindow(QMainWindow):
         button = QPushButton("Press Me!") 
         button.setCheckable(True)
         button.clicked.connect(self.the_button_was_clicked) 
+        button.toggled.connect(self.the_button_was_toggled)
 
         # Set the central widget of the Window. 
         self.setCentralWidget(button) 
 
+    # Custom slot
     def the_button_was_clicked(self): 
         print("Clicked!")   
+
+    def the_button_was_toggled(self, checked): 
+        print("Checked?", checked) 
 
 app = QApplication(sys.argv) 
 window = MainWindow() 
