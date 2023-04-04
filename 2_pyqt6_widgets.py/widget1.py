ab0f758ec13cbe7c1b01e86c9cf873fc58088e4e
jwc20 <choij2015@gmail.com>
2023-04-04 15:43:49 -0700

    Update

--- 2_pyqt6_widgets.py/widget1.py
+++ 2_pyqt6_widgets.py/widget1.py
@@ -1,17 +1,37 @@
 import sys
 from PyQt6.QtWidgets import (
-    QMainWindow, QApplication,
-    QLabel, QCheckBox, QComboBox, QListWidget, QLineEdit,
-    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider
+    QMainWindow,
+    QApplication,
+    QLabel,
+    QCheckBox,
+    QComboBox,
+    QListWidget,
+    QLineEdit,
+    QLineEdit,
+    QSpinBox,
+    QDoubleSpinBox,
+    QSlider,
 )
 from PyQt6.QtCore import Qt
 
-class MainWindow(QMainWindow):
 
+class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("My App")
+
+        widget = QLabel("Hello")
+        font = widget.font()
+        font.setPointSize(28)
+        widget.setFont(font)
+        widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # you can also set the image
+        # widget,setPixmap(QPixmap("icon.png"))
+        # widget.setScaledContents(True)
+
+        self.setCentralWidget(widget)
 
 
 app = QApplication(sys.argv)
