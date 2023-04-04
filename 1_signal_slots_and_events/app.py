70d573205a03d3d78c8ccd0cb728bf41713084e7
jwc20 <choij2015@gmail.com>
2023-04-04 14:35:35 -0700

    Store Boolean button state in variable called button_is_checked.

--- 1_signal_slots_and_events/app.py
+++ 1_signal_slots_and_events/app.py
@@ -5,12 +5,16 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 class MainWindow(QMainWindow): 
     def __init__(self): 
         super().__init__() 
+
+        self.button_is_checked = True
+
         self.setWindowTitle("My App") 
 
         button = QPushButton("Press Me!") 
         button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked) 
+        # button.clicked.connect(self.the_button_was_clicked) 
         button.toggled.connect(self.the_button_was_toggled)
+        button.setChecked(self.button_is_checked)   
 
         # Set the central widget of the Window. 
         self.setCentralWidget(button) 
@@ -20,6 +24,7 @@ class MainWindow(QMainWindow):
         print("Clicked!")   
 
     def the_button_was_toggled(self, checked): 
+        self.button_is_checked = checked
         print("Checked?", checked) 
 
 app = QApplication(sys.argv) 
