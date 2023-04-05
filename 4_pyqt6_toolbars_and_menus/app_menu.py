bf09681638d5f7eb789295f7c1edb2be7ff7f1ad
jwc20 <choij2015@gmail.com>
2023-04-05 11:05:25 -0700

    Add shortcut for button pressing.

--- 4_pyqt6_toolbars_and_menus/app_menu.py
+++ 4_pyqt6_toolbars_and_menus/app_menu.py
@@ -14,12 +14,18 @@ from PyQt6.QtWidgets import (
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
-        self.setWindowTitle("My App")
-        self.setGeometry(400, 400, 400, 400)
+        super().__init__()
 
-        label = QLabel("Menu app!")
+        self.setWindowTitle("My App")
+
+        label = QLabel("Hello!")
+
+        # The `Qt` namespace has a lot of attributes to customize
+        # widgets. See: http://doc.qt.io/qt-5/qt.html
         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
         self.setCentralWidget(label)
 
         toolbar = QToolBar("My main toolbar")
@@ -30,6 +36,13 @@ class MainWindow(QMainWindow):
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.onMyToolBarButtonClick)
         button_action.setCheckable(True)
+
+        # You can enter keyboard shortcuts using key names (e.g. Ctrl+p)
+        # Qt.namespace identifiers (e.g. Qt.CTRL + Qt.Key_P)
+        # or system agnostic identifiers (e.g. QKeySequence.StandardKey.Print)
+        button_action.setShortcut(
+            QKeySequence("Ctrl+p")
+        )  # Note: For macOS use "CMD+p".
         toolbar.addAction(button_action)
 
         toolbar.addSeparator()
@@ -38,7 +51,7 @@ class MainWindow(QMainWindow):
         button_action2.setStatusTip("This is your button2")
         button_action2.triggered.connect(self.onMyToolBarButtonClick)
         button_action2.setCheckable(True)
-        toolbar.addAction(button_action2)
+        toolbar.addAction(button_action)
 
         toolbar.addWidget(QLabel("Hello"))
         toolbar.addWidget(QCheckBox())
@@ -49,6 +62,12 @@ class MainWindow(QMainWindow):
 
         file_menu = menu.addMenu("&File")
         file_menu.addAction(button_action)
+
+        file_menu.addSeparator()
+
+        file_submenu = file_menu.addMenu("Submenu")
+
+        file_submenu.addAction(button_action2)
 
     def onMyToolBarButtonClick(self, s):
         print("click", s)
