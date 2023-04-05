735c07b8049dba3baba90eb194ef556bfb95d0e2
jwc20 <choij2015@gmail.com>
2023-04-04 20:29:28 -0700

    Add button on toolbar.

--- 4_pyqt6_toolbars_and_menus/app_toolbar.py
+++ 4_pyqt6_toolbars_and_menus/app_toolbar.py
@@ -19,6 +19,12 @@ class MainWindow(QMainWindow):
         toolbar = QToolBar("My main toolbar")
         self.addToolBar(toolbar)
 
+        button_action = QAction("Your button", self) 
+        button_action.setStatusTip("This is your button") 
+        button_action.triggered.connect(self.onMyToolBarButtonClick)
+        toolbar.addAction(button_action)
+
+
     def onMyToolBarButtonClick(self, s):
         print("click", s)
 
