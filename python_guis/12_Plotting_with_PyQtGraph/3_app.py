092071306def9e80eee96af3bb83ad0f1a6e259e
jwc20 <choij2015@gmail.com>
2023-04-06 19:21:38 -0700

    Create book code.

---
+++ python_guis/12_Plotting_with_PyQtGraph/3_app.py
@@ -0,0 +1,54 @@
+from PyQt6 import QtWidgets, QtCore
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+import sys  # We need sys so that we can pass argv to QApplication
+import os
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.graphWidget = pg.PlotWidget()
+        self.setCentralWidget(self.graphWidget)
+
+        hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
+
+        # Add Background colour to white
+        self.graphWidget.setBackground("w")
+        # Add Title
+        self.graphWidget.setTitle("Your Title Here", color="b", size="30pt")
+        # Add Axis Labels
+        styles = {"color": "#f00", "font-size": "20px"}
+        self.graphWidget.setLabel("left", "Temperature (°C)", **styles)
+        self.graphWidget.setLabel("bottom", "Hour (H)", **styles)
+        # Add legend
+        self.graphWidget.addLegend()
+        # Add grid
+        self.graphWidget.showGrid(x=True, y=True)
+        # Set Range
+        self.graphWidget.setXRange(0, 10, padding=0)
+        self.graphWidget.setYRange(20, 55, padding=0)
+
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.graphWidget.plot(
+            hour,
+            temperature,
+            name="Sensor 1",
+            pen=pen,
+            symbol="+",
+            symbolSize=30,
+            symbolBrush=("b"),
+        )
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
