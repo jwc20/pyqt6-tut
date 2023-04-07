e9042d761566436b02049fda420b99a394294dc7
jwc20 <choij2015@gmail.com>
2023-04-06 19:11:41 -0700

    Add legend.

--- python_guis/12_Plotting_with_PyQtGraph/2_app.py
+++ python_guis/12_Plotting_with_PyQtGraph/2_app.py
@@ -8,7 +8,6 @@ import os
 
 
 class MainWindow(QtWidgets.QMainWindow):
-
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
@@ -18,24 +17,39 @@ class MainWindow(QtWidgets.QMainWindow):
         hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
 
-        # self.graphWidget.setBackground('w')
-        self.graphWidget.setBackground(QtGui.QColor(100,50,254,25))
+        # Backgraound color
+        self.graphWidget.setBackground('w')
+        # self.graphWidget.setBackground(QtGui.QColor(100, 50, 254, 25))
 
-
+        # Line color
         # pen = pg.mkPen(color=(255, 0, 0))
-        pen = pg.mkPen(color=(255, 0, 0), width=2, style=QtCore.Qt.PenStyle.DashDotDotLine)
+        NewPen = pg.mkPen(
+            color=(255, 0, 0), width=2, style=QtCore.Qt.PenStyle.DashDotDotLine
+        )
 
+        # Title
         # self.graphWidget.setTitle("<span style=\"color:blue;font-size:30pt\">Your Title Here</span>")
         self.graphWidget.setTitle("Your Title Here", color="b", size="30pt")
 
-
+        # Axis
         # style = { "color": "#FFF", "font-size": "20px" }
-        style = { "color": "r", "font-size": "30pt" }
-        self.graphWidget.setLabel("left", "Temperature", **style) 
+        style = {"color": "r", "font-size": "30pt"}
+        self.graphWidget.setLabel("left", "Temperature", **style)
         self.graphWidget.setLabel("bottom", "Hour of Day", **style)
 
 
-        self.graphWidget.plot(hour, temperature, pen=pen, symbol="+", symbolSize=30, symbolBrush=("b"))
+        # Plot
+        self.graphWidget.addLegend()
+        self.graphWidget.plot(
+            hour,
+            temperature,
+            name="Sensor 1",
+            pen=NewPen,
+            symbol="+",
+            symbolSize=30,
+            symbolBrush=("b"),
+        )
+
 
 
 def main():
