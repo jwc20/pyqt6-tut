162d8cf8b9e2bbc48ec662bc467defe084f590e3
jwc20 <choij2015@gmail.com>
2023-04-06 18:32:49 -0700

    Change background color.

--- python_guis/12_Plotting_with_PyQtGraph/0_app.py
+++ python_guis/12_Plotting_with_PyQtGraph/0_app.py
@@ -1,4 +1,4 @@
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets, QtGui
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
@@ -17,6 +17,13 @@ class MainWindow(QtWidgets.QMainWindow):
         temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
 
         # plot data: x, y values
+        # self.graphWidget.setBackground('w')
+        # self.graphWidget.setBackground('#bbccaa')
+        # self.graphWidget.setBackground((100,50,255))      # RGB each 0-255
+        # self.graphWidget.setBackground((100,50,255,25))
+
+        self.graphWidget.setBackground(QtGui.QColor(100,50,254,25))
+
         self.graphWidget.plot(hour, temperature)
 
 
