4f82a6cc13217ba44e7ba4e5312610202ea07a0d
jwc20 <choij2015@gmail.com>
2023-04-08 17:04:20 -0700

    Create plot graph app using custom widget.

---
+++ python_guis/16_Embedding_custom_widgets_from_Qt_Designer/app.py
@@ -0,0 +1,23 @@
+from PyQt6 import QtWidgets, uic 
+from pyqtgraph import PlotWidget
+
+import pyqtgraph as pg 
+import sys 
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        uic.loadUi('mainwindow.ui', self)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
+
