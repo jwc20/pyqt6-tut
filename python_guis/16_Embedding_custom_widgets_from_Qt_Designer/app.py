274b970123e7d2df58569ccf119dd5bab476c1e1
jwc20 <choij2015@gmail.com>
2023-04-08 17:05:49 -0700

    Add data points to plot graph.

--- python_guis/16_Embedding_custom_widgets_from_Qt_Designer/app.py
+++ python_guis/16_Embedding_custom_widgets_from_Qt_Designer/app.py
@@ -9,6 +9,11 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi('mainwindow.ui', self)
+        self.plot([1,2,3,4,5,6,7,8,9,10], [30,32,34,32,33,31,29,32,35,45])
+
+    
+    def plot(self, x, y):
+        self.graphWidget.plot(x, y)
 
 
 def main():
